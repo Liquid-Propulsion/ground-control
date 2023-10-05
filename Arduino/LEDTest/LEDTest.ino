8fe7f2b47c86e9da8e74686af395fb367f070011
Leo P <leo@myriad-labs.com>
2023-10-04 20:16:56 -0400

    Update LEDTest.ino
    fSequence() run when letter 'f' detected; otherwise, proceeds normally

--- Arduino/LEDTest/LEDTest.ino
+++ Arduino/LEDTest/LEDTest.ino
@@ -1,5 +1,17 @@
 const int OutputPin = 8;
 
+void fSequence(int OutputPin) {
+  int x = 1;
+  // spend about 6 seconds repeating between high and low, first really fast, then slowing down
+  while (x < 18) {
+    digitalWrite(OutputPin, HIGH);
+    delay(x*10);
+    digitalWrite(OutputPin, LOW);
+    delay((x+1)*10);
+    x++;
+  }
+}
+
 void setup() {
   // Declare output pin as an output
   pinMode(OutputPin, OUTPUT);
@@ -11,21 +23,29 @@ void setup() {
 
 
 void loop() {
+  // INPUTS //
   // Wait for serial availability if it is lost
   while (Serial.available() == 0) {
   }
 
-  // Send a high signal to the output pin when any key is pressed
-  digitalWrite(OutputPin, HIGH);
-
   // Read the incoming character from the serial port,
   char IncomingByte = Serial.read();
-    
+
   // and print it to serial.
   Serial.print("Received: ");
   Serial.println(IncomingByte);
 
-  // Wait for a short duration (adjust as needed)
+
+  
+  // OUTPUTS //
+  if (IncomingByte == 'f') {
+    fSequence(OutputPin);
+  } else {
+    // Send a basic high signal to the output pin when any key is pressed
+    digitalWrite(OutputPin, HIGH);
+  }
+
+  // Wait for a short duration before continuing (adjust as needed)
   delay(10);
 
   // Set the output pin LOW after a delay to control the duration
